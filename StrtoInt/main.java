dceaf217e3117d4c72d625dbd54c28680b001b44
ArcticFoox <jongsin16@naver.com>
2023-12-08 23:22:59 +0900

    12925

---
+++ StrtoInt/main.java
@@ -0,0 +1,17 @@
+package StrtoInt;
+
+import java.util.Scanner;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
+
+public class main {
+    public static void main(String args[]){
+
+    }
+}
